90303c3c7c70b358ae8260792cb12a01544db5ae
mark <mmarkmarkm0@gmail.com>
2024-10-30 21:14:35 +0300

    functionality to add a new recipe:

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,23 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
     @ingredient = Ingredient.new
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe, notice: "You have created a recipe"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
 end
