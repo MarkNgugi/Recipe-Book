41b4d4f2d76795d7ef5ff16ee3796f535c024f8b
mark <mmarkmarkm0@gmail.com>
2024-10-30 20:04:28 +0300

    index and show functionalities added:

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,10 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @ingredient = Ingredient.new
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
   has_many :ingredients, dependent: :destroy
   validates :name, presence: true
-  validate :description, presence: true
+  validates :description, presence: true
 end
